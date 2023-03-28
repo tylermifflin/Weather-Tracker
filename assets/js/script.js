c9e41938c8278435527d3524b849d716520789dd
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-28 15:07:11 -0600

    added an id for getweatherbtn in html and a var queryselector to use it in JS,made an event listener for the getweaterbtn to take the user input for city and state and run getWeather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // created variables for my API key, city, state, and queryURL, so when the user search a specific city or state, the API will return the weather data for that city
-var APIkey = "21ebfd56b0f5a31d68ebf78b983534f6"
+var APIkey = "44c0ceeaad2f13d35c6b6f46923f9c42"
 var city; 
 var state;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + APIkey;
+var getweatherbtn = document.querySelector("#getweatherbtn");
 
 // function to get weather using fetch 
 function getWeather() {
@@ -14,4 +15,13 @@ function getWeather() {
         console.log(data);
     });
 }
-// how to display a 5 day forecast for the city that the user searched
+
+// event listener for search button
+getweatherbtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    city = document.querySelector("#city").value;
+    state = document.querySelector("#state").value;
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + APIkey;
+    getWeather();
+}
+);
