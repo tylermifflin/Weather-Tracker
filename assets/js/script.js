592aee9b6a03cd21a83d2330b9f6a814c0409817
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-28 14:10:11 -0600

    created a function to getWeather using the fetch method with my var queryURL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
+// created variables for my API key, city, state, and queryURL, so when the user search a specific city or state, the API will return the weather data for that city
 var APIkey = "21ebfd56b0f5a31d68ebf78b983534f6"
 var city; 
 var state;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + APIkey;
+
+// function to get weather using fetch 
+function getWeather() {
+    fetch(queryURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
+}
