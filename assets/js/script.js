d9295469fa90d46be9ec9af66756eec75dfbf3b1
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-28 14:03:46 -0600

    created a variable for city, state, and queryURL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
 var APIkey = "21ebfd56b0f5a31d68ebf78b983534f6"
+var city; 
+var state;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + APIkey;
