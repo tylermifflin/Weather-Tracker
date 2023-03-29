caec0f9ca18ef5f227d85bf34914a3000f8fb694
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-28 18:30:36 -0600

    got my city URL to work at least for now, still gotta figure out how to change it back to lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 // created variables for my API key, city, state, and queryURL, so when the user search a specific city or state, the API will return the weather data for that city
 var APIkey = "44c0ceeaad2f13d35c6b6f46923f9c42"
-var city; 
-var state;
+
 var lat;
 var lon;
+var country = "US";
+
 // need my queryURL to be a variable with lat and lon, so when the user search a specific city or state, the API will return the weather data for that city
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIkey;
 var getweatherbtn = document.querySelector("#getweatherbtn");
 
 // function to get weather using fetch 
-function getWeather() {
-    fetch(queryURL)
+function getWeather(cityURL) {
+    fetch(cityURL)
     .then(function(response) {
         return response.json();
     })
@@ -19,12 +21,12 @@ function getWeather() {
     });
 }
 
+
 // event listener for search button
 getweatherbtn.addEventListener("click", function(event) {
     event.preventDefault();
-    city = document.querySelector("#city").value;
-    state = document.querySelector("#state").value;
-    queryURL = 
-    getWeather();
-}
-);
+    var city = document.querySelector("#city").value;
+    var  cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIkey;
+    console.log(city);
+    getWeather(cityURL);
+});
