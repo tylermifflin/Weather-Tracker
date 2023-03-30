4c2d88dd8dff8f3df60a295a0efc4ff4eec5fb2b
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-29 19:45:16 -0600

    at least i got my console log to work for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // created variables for my API key, city, latitude and longitude, so when the user search a specific city or state, the API will return the weather data for that city
 var APIkey = "44c0ceeaad2f13d35c6b6f46923f9c42"
 var city = document.querySelector("#city").value;
-var weatherdata = document.querySelector("#weatherdata");
-var cityname = document.querySelector("#cityname");
-var date = document.querySelector("#date");
-var weathericon = document.querySelector("#weathericon");
-var temperature = document.querySelector("#temperature");
-var humidity = document.querySelector("#humidity");
-var windspeed = document.querySelector("#windspeed");
 var lat, lon;
+var weatherdataEl = document.querySelector(".card-body");
+var citynameEl = document.querySelector("#cityname");
+var dateEl = document.querySelector("#date");
+var weathericonEl = document.querySelector("#weathericon");
+var temperatureEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var windspeedEl = document.querySelector("#windspeed");
 
 // need my queryURL to be a variable with lat and lon, so when the user search a specific city or state, the API will return the weather data for that city
 var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIkey;
@@ -40,8 +40,22 @@ function getWeather(queryURL) {
         return response.json();
     })
     .then(function(data) {
+        //console log data to see if it is working, creating variables for the data I want to display
         console.log(data);
-        // loop through the data list and display the weather data for the current day and the next 5 days, displaying the city name, date, weather icon, temperature, humidity, and wind speed
+        var cityname = data.city.name;
+        var date = data.list[0].dt_txt;
+        var weathericon = data.list[0].weather[0].icon;
+        var temperature = data.list[0].main.temp;
+        var humidity = data.list[0].main.humidity;
+        var windspeed = data.list[0].wind.speed;
+        console.log(cityname);
+        console.log(date);
+        console.log(weathericon);
+        console.log(temperature);
+        console.log(humidity);
+        console.log(windspeed);
+        
+       
 
     });
 }
