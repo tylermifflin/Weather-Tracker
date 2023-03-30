e99267baa6afaf1915b17b324c68178d661c2ad8
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-29 20:04:41 -0600

    basically gone in circles for hours making absolutley no progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,6 @@
 var APIkey = "44c0ceeaad2f13d35c6b6f46923f9c42"
 var city = document.querySelector("#city").value;
 var lat, lon;
-var weatherdataEl = document.querySelector(".card-body");
-var citynameEl = document.querySelector("#cityname");
-var dateEl = document.querySelector("#date");
-var weathericonEl = document.querySelector("#weathericon");
-var temperatureEl = document.querySelector("#temperature");
-var humidityEl = document.querySelector("#humidity");
-var windspeedEl = document.querySelector("#windspeed");
 
 // need my queryURL to be a variable with lat and lon, so when the user search a specific city or state, the API will return the weather data for that city
 var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIkey;
@@ -34,33 +27,35 @@ function getCity(cityURL) {
 }
 
 // function to get weather using fetch for the queryURL, using the lat and lon from the getCity function
+
 function getWeather(queryURL) {
     fetch(queryURL)
-    .then(function(response) {
+      .then(function (response) {
         return response.json();
-    })
-    .then(function(data) {
-        //console log data to see if it is working, creating variables for the data I want to display
-        console.log(data);
+      })
+      .then(function (data) {
         var cityname = data.city.name;
         var date = data.list[0].dt_txt;
         var weathericon = data.list[0].weather[0].icon;
         var temperature = data.list[0].main.temp;
         var humidity = data.list[0].main.humidity;
         var windspeed = data.list[0].wind.speed;
-        console.log(cityname);
-        console.log(date);
-        console.log(weathericon);
-        console.log(temperature);
-        console.log(humidity);
-        console.log(windspeed);
-        
-       
-
-    });
-}
-
-
+  
+        var citynameEl = document.querySelector("#cityname");
+        var dateEl = document.querySelector("#date");
+        var weathericonEl = document.querySelector("#weathericon");
+        var temperatureEl = document.querySelector("#temperature");
+        var humidityEl = document.querySelector("#humidity");
+        var windspeedEl = document.querySelector("#windspeed");
+  
+        citynameEl.textContent = cityname;
+        dateEl.textContent = date;
+        weathericonEl.src = "https://openweathermap.org/img/w/" + weathericon + ".png";
+        temperatureEl.textContent = temperature + "°F";
+        humidityEl.textContent = humidity + "%";
+        windspeedEl.textContent = windspeed + " mph";
+      });
+  }
 
 // event listener for search button
 getweatherbtn.addEventListener("click", function(event) {
@@ -71,3 +66,5 @@ getweatherbtn.addEventListener("click", function(event) {
     getCity(cityURL);
 });
 
+
+  
